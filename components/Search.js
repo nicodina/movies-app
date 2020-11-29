380cf16d580cb21edc00d39aa8f63474c2946138
ndinardo <nicola.dinardo@ticketeasy.it>
2020-11-29 19:19:09 +0100

    infinite scrolling

--- components/Search.js
+++ components/Search.js
@@ -7,26 +7,78 @@ export default function Search() {
     
     const [state, getMovies] = useState({
       movies: [],
-      isLoading: false,
     });
-    searchText = '';
 
+    const [searchText, setSearchText] = useState('')
+    const [page, setPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
 
-    function _loadFilms(){
-      getMovies({isLoading: true})
+    return (
+      <View style={styles.container} >
+        <Text style={styles.title}>MoviesAndMe</Text>
+        <Text style={styles.text}>What movie would you like to watch tonight?</Text>
+        <TextInput style={styles.textinput} onSubmitEditing={() => {
+          _searchMovie()
+          }} onChangeText={(text) => _searchTextChanged(text)} placeholder='Type the title' />
+        <Button style={styles.button} title='Search' onPress={() => {
+          _searchMovie();
+          }}/>
+
+        <FlatList
+          data={state.movies}
+          keyExtractor={(item) => item.id.toString()}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            console.log('onEndReached');
+            console.log(page);
+            console.log(totalPages);
+            if (page < totalPages) {
+              _loadFilms();
+            }
+          }}
+          renderItem={({item}) => <MovieItem movie={item}/>}
+        />
+
+        {_displayLoading()}
+      </View>
+    );
+    
+    function _searchMovie() {
+      const zero = 0;
+      getMovies({
+        movies: [],
+      });
+      setPage(0);
+      setTotalPages(0);
+
+      _loadFilms();
+    }
+
+    function _loadFilms() {
+      setIsLoading(true);
+      console.log("search text length: " + searchText.length)
+      console.log('AAAAAAA ' + page)
+
       if (searchText.length > 0) {
-        getMoviesFromText(searchText).then(data => getMovies(
-          {movies: data.results, isLoading: false})
-        );
+
+        getMoviesFromText(searchText, page + 1).then(data => {
+          getMovies({
+            movies: [...state.movies, ...data.results], 
+          });
+          setIsLoading(false);
+          setPage(data.page);
+          setTotalPages(data.total_pages);
+        });
       }
     }
 
-    function _searchTextChanged(text){
-      searchText = text;
+    function _searchTextChanged(text) {
+      setSearchText(text)
     }
 
     function _displayLoading() {
-      if (state.isLoading === true) {
+      if (isLoading === true) {
         return (
           <View style={styles.loading_container}>
             <ActivityIndicator size='large' />
@@ -35,22 +87,7 @@ export default function Search() {
       }
     }
 
-    return (
-      <View style={styles.container} >
-        <Text style={styles.title}>MoviesAndMe</Text>
-        <Text style={styles.text}>What movie would you like to watch tonight?</Text>
-        <TextInput style={styles.textinput} onSubmitEditing={() => _loadFilms()} onChangeText={(text) => _searchTextChanged(text)} placeholder='Type the title' />
-        <Button style={styles.button} title='Search' onPress={() => _loadFilms()}/>
 
-        <FlatList
-          data={state.movies}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <MovieItem movie={item}/>}
-        />
-
-        {_displayLoading()}
-      </View>
-    );
 }
 
 const styles = StyleSheet.create({
--- services/tmdb.connector.js
+++ services/tmdb.connector.js
@@ -1,8 +1,9 @@
 import {BASE_URL, BASE_URL_IMAGE, API_KEY} from '../config/tmdb.config'
 
-export function getMoviesFromText(text) {
+export function getMoviesFromText(text, page) {
 
-    const url = BASE_URL + '?api_key=' + API_KEY + '&language=en&query=' + text;
+    const url = BASE_URL + '?api_key=' + API_KEY + '&language=en&query=' + text + '&page=' + page;
+    console.log(url)
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.log(error)) 
